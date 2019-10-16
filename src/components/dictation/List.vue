8b287d45302b0a6cbb5fb4692338d82786ba1552
Yury Sosnov <yury.v.sosnov@hotmail.com>
2019-10-16 23:56:16 +0300

    Fix mistake in code )

--- src/components/dictation/List.vue
+++ src/components/dictation/List.vue
@@ -93,7 +93,7 @@ export default {
                 if (element.id === data.id) {
                     this.items.splice(index,1);
                     return false;
-                };
+                }
             });
         },
     },
